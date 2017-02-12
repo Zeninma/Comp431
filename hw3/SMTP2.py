a91575ca6cec62f6d00fb59ea083f6fcb0d3bc6f
Ao Zeng <13871299198@msn.cn>
2017-02-12 13:37:11 -0500

    start doing hw3

---
+++ hw3/SMTP2.py
@@ -0,0 +1,83 @@
+import sys
+import pdb
+
+SUCCESS1 = ['250','354']
+
+class SuperEnum(object):
+    '''
+    an Enum built for Python 2.7
+    original source:
+    http://codereview.stackexchange.com/questions/109724/yet-another-lightweight-enum-for-python-2-7
+    '''
+    class __metaclass__(type):
+        def __iter__(self):
+            for item in self.__dict__:
+                if item == self.__dict__[item]:
+                    yield item
+
+class CommandType(SuperEnum):
+    '''
+    Enum for command type
+    '''
+    MAIL_FROM = 1
+    RCPT = 2
+    DATA = 3
+    NEWSTART = 4
+
+class ReaderState(SuperEnum):
+    '''
+    an ENUM for ForwardFileReader state
+    '''
+    LISTEN = 1
+    DATA_MODE = 2
+
+########################################################################
+#######################Above are helper class, mainly ENUM##############
+########################################################################
+
+class ForwardFileReader():
+    '''
+    Read a forward file, and then generate SMTP command correspondingly.
+    After ouputing each command, pause and wait for SMTP server respond.
+    Quit if there is any error or reaching the end of the forward file.
+    '''
+    def __init__(self, file_name):
+        '''
+        constructor for Forwar_file_reader
+        '''
+        self.state = ReaderState.LISTEN
+        file = open(file_name, "r")
+        if file.mode == 'r':
+            self.lines = file.readline()
+
+    def type_check(self, line):
+        '''
+        check the command type of the input line
+        and return it
+        '''
+        line_len = len(line)
+        if self.state == ReaderState.LISTEN:
+            if line_len > 4:
+                if line[0:5] == 'From:':
+                    return CommandType.MAIL_FROM
+            if line_len > 2:
+                if line[0:3] == 'To:':
+                    return CommandType.RCPT
+            else:
+                #the original document has error
+                return CommandType.DATA
+        else:
+            if line_len > 4:
+                if line[0:5] == 'From:':
+                    return CommandType.NEWSTART
+            else:
+                return CommandType.DATA
+
+    def start(self):
+        '''
+        start processing forward file
+        '''
+        for line in self.lines:
+            cmd_type = self.type_check(line)
+        # out of the for loop, need to type the end Command
+        # for the DATA part
